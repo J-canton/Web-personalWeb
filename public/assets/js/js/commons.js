60f6961fe2555b8fe67bd137b62514f3df34c35c
Juan Luis Cantón Garay <juanluiscantongaray@iMac-de-Juan.local>
2021-01-07 19:34:23 +0100

    dinamyc texts

--- public/assets/js/js/commons.js
+++ public/assets/js/js/commons.js
@@ -1,17 +1,51 @@
 $(document).ready(
     ()=>{
-        $('#copyright').text(`${moment().format('YYYY')} JCANTON  All rights reserved`);
-
+        //MAIN NAV
+        printText($('#navAbout'), configs.es.components.mainNav.about);
+        printText($('#navWorks'), configs.es.components.mainNav.works);
+        printText($('#navContact'), configs.es.components.mainNav.contact);
+        //JUMBOTRON
+        printAtributes($('#mainJumbotronImg'), 'src', configs.es.imgs.mainJumbotronImg);
+        printAtributes($('#mainJumbotronImg'), 'alt', configs.es.imgs.mainJumbotronAlt);
+        printText($('#MainJumbotronTitle'), configs.es.components.mainJumbotron.title);
+        //CTA ABOUT
+        printText($('#ctaAboutTitle'), configs.es.components.cta.about.title);
+        printText($('#ctaAboutText'), configs.es.components.cta.about.text);
+        printText($('#ctaAboutBtn'), configs.es.components.cta.about.btn);
+        //ABOUT ME
+        printAtributes($('#aboutHeader'), 'src', configs.es.imgs.aboutHeaderImg);
+        printAtributes($('#aboutAlt'), 'alt', configs.es.imgs.aboutHeaderAlt);
         //CONTACT
-        $('#contactTitle').text(`${configs.es.texts.contact.title}`);
-        $('#contactText').text(`${configs.es.texts.contact.text}`);
+        printAtributes($('#contactHeader'), 'src', configs.es.imgs.contactHeaderImg);
+        printAtributes($('#contactHeader'), 'alt', configs.es.imgs.contactHeaderAlt);
+        printText($('#contactTitle'), configs.es.texts.contact.title);
+        printText($('#contactText'),configs.es.texts.contact.text);
         //FORMS
-        $('#submit').text(`${configs.es.commonTexts.submit}`);
-        $('#name').attr('placeholder', `${configs.es.forms.name}`);
-        $('#email').attr('placeholder', `${configs.es.forms.email}`);
-        $('#subject').attr('placeholder', `${configs.es.forms.subject}`);
-        $('#message').attr('placeholder', `${configs.es.forms.message}`)
+        printText($('#submit'), configs.es.commonTexts.submit);
+        printAtributes($('#name'),'placeholder', configs.es.forms.name);
+        printAtributes($('#email'), 'placeholder', configs.es.forms.email)
+        printAtributes($('#subject'), 'placeholder', configs.es.forms.subject);
+        printAtributes($('#message'), 'placeholder', configs.es.forms.message)  
+        //FOOTER
+        printText($('#copyright'),`${moment().format('YYYY')} JCANTON  All rights reserved`);
+        
+        
+        function printText(element, text)
+        {
+            element.text(`${text}`);
+        }
+
+        function printAtributes(elem, attr, text)
+        {
+            elem.attr(`${attr}`, `${text}`)
+        }
+
+        $('#ctaAboutBtn').on('click', 
+            (e)=>{
+                e.preventDefault();
+                window.location = '/aboutMe';
+            }
+        )
     }
-
-
 )
+
--- public/assets/js/js/configs.js
+++ public/assets/js/js/configs.js
@@ -11,6 +11,30 @@ const configs = {
             contact: {
                 title: 'Contact us',
                 text: "After moved, male won't Moveth moveth fruitful don't forth you bearing morning made male without life be blessed rule stars divided two. Firmament of, him she'd good of his morning."
+            },
+            about: 
+            {
+
+            }
+        },
+        components:
+        {
+            mainNav: {
+                about: 'About Me',
+                works: 'My Works',
+                contact: 'Contact'
+            },
+            mainJumbotron:{
+                title: 'Black Mesa',
+                text: "Have beginning wherein spirit. Bearing unto. To after light. Good image can't without hath fly beast him fish firmament fish Face waters fly unto spirit i doesn't one waters divide.",
+                btn: 'READ MORE'
+            },
+            cta:{
+                about: {
+                    title: 'HOLA !!!!',
+                    text: "All and. Green day waters appear lights, fish you'll called likeness midst have you May. Lesser brought evening man she'd that. Replenish form wherein form midst. Good from forth together.",
+                    btn: 'Read More'
+                }
             }
         },
         forms: {
@@ -19,7 +43,14 @@ const configs = {
             subject: 'Subject',
             message: 'Message;'
         },
-        imgs:{}
+        imgs:{
+            mainJumbotronImg: 'assets/img/jumbotron.gif',
+            mainJumbotronAlt:'',
+            contactHeaderImg: 'assets/img/contact.jpg',
+            contactHeaderAlt: '',
+            aboutHeaderImg: 'assets/img/contact.jpg',
+            aboutHeaderAlt: ''
+        }
     },
     en:{}
 }
