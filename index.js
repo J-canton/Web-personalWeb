8c0be06c48eb4d18245296fb214cf2069ca8cb46
Juan Luis Cantón Garay <juanluiscantongaray@iMac-de-Juan.local>
2021-01-11 09:33:04 +0100

    some changes2

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const emailSender = require('./app/controller/mailController');
+const fs = require('fs');
+const csv = require('csv-parser');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;  
+const moment = require('moment');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+const csvWriter = createCsvWriter(
+    {
+        path: 'data.csv',
+        header: [
+            {id: 'date', title:'Date'},
+            {id: 'name', title: 'Name'},
+            {id: 'email', title: 'Email'},
+            {id: 'subject', title: 'Subject'},
+            {id: 'msg', titile: 'Msg'}
+        ]
+    }
+)
+
+function readCsv(){
+    fs.createReadStream('data.csv')
+        .pipe(csv())
+        .on('data', 
+        (row)=>{
+            console.log(row);
+        }
+    ).on('end', ()=>{
+        console.log('CSV PROCESSED');
+    })
+}
+
+function writeCsv(){
+    csvWriter.writeRecords(data).then(()=>{console.log('CSV WRITTEN')})
+}
+
+
+let data = [
+    {
+        date: moment().format('Do MMMM  YYYY, h:mm:ss a'),
+        name: 'Mismo',
+        email: 'mismo@mismo.com',
+        subject: 'mismo',
+        msg: 'Lorem ipsum dolor sit amet...'
+    }
+]
+
+
 
 
 app.get('/', 
@@ -37,9 +82,18 @@ app.get('/games/arkanoid',
     }
 )
 
-app.post('/sendMail', (req,res)=>{
-  emailSender.sendEmail(req,res);  
-})
+app.post('/sendMail', 
+    (req,res)=>{
+        console.log('SEND');
+        writeCsv();
+    }
+);
+
+app.get('/getMail', 
+    (req,res)=>{
+        console.log('GET');
+        readCsv();
+    })
 
 app.use(
     (req,res,next)=>{
